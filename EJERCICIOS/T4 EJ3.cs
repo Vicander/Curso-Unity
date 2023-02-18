9f9b5afcaf888002cafb4c264202f1fa2f1319b1
Vicander <vdvideojuego@gmail.com>
2023-02-18 19:19:52 +0100

    Completasao 2.0

---
+++ EJERCICIOS/T4 EJ3.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pregunta_2 : MonoBehaviour
+{
+    private int MultiplicaDosEnteros (int a , int b){
+        int resultado;
+        resultado = a * b;
+        return resultado;
+    }
+    int resultado = MulplicaDosEnteros (10, 3);
+}
