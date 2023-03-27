e15ab393252793a34e0664ea408cd9f8c5bc63db
Vicander <vdvideojuego@gmail.com>
2023-03-27 14:44:00 +0200

    Collectado

--- EJERCICIOS/T5/T5 EJ5.cs
+++ EJERCICIOS/T5/T5 EJ5.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,28 +21,3 @@ public class GetCoin : MonoBehaviour
         }
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class GetCoin : MonoBehaviour
-{
-
-    public int CoinNumber;
-    void Start()
-    {
-        CoinNumber = 0;
-    }
-
-    private void OnTriggerEnter2D (Collider2D collider2D)
-    {
-        if (collider2D.gameObject.CompareTag("coin"))
-        {
-            Destroy (collider2D.gameObject);
-            CoinNumber++;
-        }
-    }
-}
->>>>>>> 5511fb924daf4822ac6c15d2218eb56a3c1b421d
